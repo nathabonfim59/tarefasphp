61139557818accbe022f43fb7ee6600ac70b7e2f
nathabonfim59 <nathabonfim59@localhost.com>
2018-11-05 08:24:08 -0200

    Adicionadas novas propriedades tarefas

--- tarefas.php
+++ tarefas.php
@@ -2,8 +2,32 @@
 
 session_start();
 
-if (isset($_GET['nome'])) {
-    $_SESSION['lista_tarefas'][] = $_GET['nome'];
+if (isset($_GET['nome']) && $_GET['nome'] != '') {
+    $tarefa = array();
+    
+    $tarefa['name'] = $_GET['nome'];
+
+    if (isset($_GET['descricao']) && $_GET['descricao'] != '') {
+        $tarefa['descricao'] = $_GET['descricao'];
+    } else {
+        $tarefa['descricao'] = '';
+    }
+
+    $tarefa['prioridade'] = $_GET['prioridade'];
+
+    if (isset($_GET['prazo']) && $_GET['prazo'] != '') {
+        $tarefa['prazo'] = $_GET['prazo'];
+    } else {
+        $tarefa['prazo'] = '';
+    }
+
+    if (isset($_GET['concluida']) && $_GET['concluida'] != '') {
+        $tarefa['concluida'] = $_GET['concluida'];
+    } else {
+        $tarefa['concluida'] = '';
+    }
+    
+    $_SESSION['lista_tarefas'][] = $tarefa;
 }
 
 if (isset($_SESSION['lista_tarefas'])) {
